3fdb3a3c661265c8020c111014d2f22a7ae2bbed
Rafael dela Cruz <Rafael.Cruz@merkle.com>
2024-03-16 13:09:04 +0800

    commit(fed): initial setup

---
+++ the-orchard/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    test
+  </main>
+</template>
+
+<style scoped></style>
---
+++ the-orchard/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
